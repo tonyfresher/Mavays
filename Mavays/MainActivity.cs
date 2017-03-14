4e529e063d825494a49a178f7d6e957b05c340cd
Tony Frehser <farnetsohn@gmail.com>
2017-03-14 22:43:53 +0500

    первый коммит
    все что нужно настроено, показывает синий экран

---
+++ Mavays/MainActivity.cs
@@ -0,0 +1,17 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace Mavays
+{
+    [Activity(Label = "Mavays", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
